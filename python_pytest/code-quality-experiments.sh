42949645da630ae5cdc1f819cb8b53085294310c
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-01-08 16:20:53 +0000

    chore: added ruff to script.

--- python_pytest/code-quality-experiments.sh
+++ python_pytest/code-quality-experiments.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 function pylint_based_code_quality() {
-  pipenv run pylint "$1"
+  pipenv run pylint "$1"  --output-format=json
+}
+
+function ruff_based_code_quality() {
+  pipenv run ruff check "$1" --config supermarketreceipt-refactoring.toml
 }
