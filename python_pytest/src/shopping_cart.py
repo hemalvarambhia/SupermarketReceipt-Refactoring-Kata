334a596f660321600e75b630699f3dd388e2899f
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-01-08 14:10:19 +0000

    refactor: tighten encapsulation of Offer, handle_offers has no idea about Offer's internal structure.

--- python_pytest/src/shopping_cart.py
+++ python_pytest/src/shopping_cart.py
@@ -59,7 +59,7 @@ class ShoppingCart:
                     discount = Discount(p, str(offer.argument) + "% off",
                                         -quantity * unit_price * offer.argument / 100.0)
 
-                if offer.offer_type is SpecialOfferType.FIVE_FOR_AMOUNT and quantity_as_int >= 5:
+                if offer.five_for_amount() and quantity_as_int >= 5:
                     discount_total = unit_price * quantity - (
                                 offer.argument * number_of_x + quantity_as_int % 5 * unit_price)
                     discount = Discount(p, str(x) + " for " + str(offer.argument), -discount_total)
