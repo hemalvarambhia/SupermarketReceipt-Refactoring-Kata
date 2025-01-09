cf5e77437f6bf51a11777bbdb427fa338113392f
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-01-09 17:42:35 +0000

    chore: added ruff to the kata.

--- python_pytest/code_quality_experiments/code-quality-experiments.sh
+++ python_pytest/code_quality_experiments/code-quality-experiments.sh
@@ -3,3 +3,7 @@
 function pylint_based_code_quality() {
   pipenv run pylint "$1"
 }
+
+function ruff_based_code_quality() {
+  pipenv run ruff check "$1" --config code_quality_experiments/supermarketreceipt-refactoring.toml
+}
