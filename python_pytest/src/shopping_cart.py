262361a3e46d704117935c9ea103b43809b91ac2
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-01-09 12:07:36 +0000

    chore: added documentation to the handle_offers method - rated at 8.75/10 (previous run: 8.54/10, +0.21).

--- python_pytest/src/shopping_cart.py
+++ python_pytest/src/shopping_cart.py
@@ -28,6 +28,10 @@ class ShoppingCart:
             self._product_quantities[product] = quantity
 
     def handle_offers(self, receipt, offers, catalog):
+        """
+        Using the offers available on products, the shopping cart looks at how many qualifying
+        products were purchased and uses the unit price to calculate the discount on them.
+        """
         for p, quantity in self._product_quantities.items():
             if p in offers.keys():
                 offer = offers[p]
