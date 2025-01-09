6b04a8c09545f3625c8b37646812715bb56cb2e4
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-01-09 11:41:38 +0000

    refactor: use early returns - 8.57/10 (previous run: 8.60/10, -0.03).

--- python_pytest/src/shopping_cart.py
+++ python_pytest/src/shopping_cart.py
@@ -40,23 +40,22 @@ class ShoppingCart:
         """
         Calculates the discount available from the offer on a qualifying product.
         """
-        discount = None
         if offer.is_two_for_amount():
             if int(quantity) >= 2:
                 total = offer.argument * (int(quantity) / 2) + int(quantity) % 2 * unit_price
                 discount_amount = unit_price * quantity - total
-                discount = Discount(p, "2 for " + str(offer.argument), -discount_amount)
+                return Discount(p, "2 for " + str(offer.argument), -discount_amount)
         if offer.is_ten_percent_discount():
             percent = offer.argument
-            discount = self.ten_percent_off(percent, p, quantity, unit_price)
+            return self.ten_percent_off(percent, p, quantity, unit_price)
         if offer.is_three_for_two() and int(quantity) > 2:
-            discount = self.three_for_two_on(p, quantity, unit_price)
+            return self.three_for_two_on(p, quantity, unit_price)
         if offer.is_five_for_amount() and int(quantity) >= 5:
             number_of_x = math.floor(int(quantity) / 5)
             discount_amount = unit_price * quantity - (
                     offer.argument * number_of_x + int(quantity) % 5 * unit_price)
-            discount = Discount(p, str(5) + " for " + str(offer.argument), -discount_amount)
-        return discount
+            return Discount(p, str(5) + " for " + str(offer.argument), -discount_amount)
+        return None
 
     def three_for_two_on(self, p, quantity, unit_price):
         """Computes a three for two discount on product"""
