399318c800d47f21bdcb1e8ef2ef9d2a1b160eee
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-01-10 14:34:46 +0000

    documentation: added comment to show how to get JSON output.

--- python_pytest/code_quality_experiments/code_quality_experiments.sh
+++ python_pytest/code_quality_experiments/code_quality_experiments.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 function ruff_based_code_quality() {
+  # --output-format=json yields JSON format e.g.
+  # pipenv run ruff check path/to/file --config path --output-format=json
   pipenv run ruff check "$1" --config code_quality_experiments/supermarketreceipt-refactoring-kata.toml
 }
