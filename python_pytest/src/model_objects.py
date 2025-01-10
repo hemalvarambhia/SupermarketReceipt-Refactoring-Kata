c095b1a4f33e11e45ee139af9b958f37ceb662ac
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-01-10 17:17:42 +0000

    refactor: inlined references to the order argument.

--- python_pytest/src/model_objects.py
+++ python_pytest/src/model_objects.py
@@ -33,23 +33,23 @@ class Offer:
 
     def calculate_discount_on(self, p, offer, quantity, unit_price):
         discount = None
-        if offer.is_two_for_amount(int(quantity)):
-            total = offer.argument * (int(quantity) / 2) + int(quantity) % 2 * unit_price
+        if self.is_two_for_amount(int(quantity)):
+            total = self.argument * (int(quantity) / 2) + int(quantity) % 2 * unit_price
             discount_n = unit_price * quantity - total
-            discount = Discount(p, "2 for " + str(offer.argument), -discount_n)
-        if offer.offer_type == SpecialOfferType.THREE_FOR_TWO and int(quantity) > 2:
+            discount = Discount(p, "2 for " + str(self.argument), -discount_n)
+        if self.offer_type == SpecialOfferType.THREE_FOR_TWO and int(quantity) > 2:
             number_of_x = math.floor(int(quantity) / 3)
             discount_amount = quantity * unit_price - (
                     (number_of_x * 2 * unit_price) + int(quantity) % 3 * unit_price)
             discount = Discount(p, "3 for 2", -discount_amount)
-        if offer.offer_type == SpecialOfferType.TEN_PERCENT_DISCOUNT:
-            discount = Discount(p, str(offer.argument) + "% off",
-                                -quantity * unit_price * offer.argument / 100.0)
-        if offer.offer_type == SpecialOfferType.FIVE_FOR_AMOUNT and int(quantity) >= 5:
+        if self.offer_type == SpecialOfferType.TEN_PERCENT_DISCOUNT:
+            discount = Discount(p, str(self.argument) + "% off",
+                                -quantity * unit_price * self.argument / 100.0)
+        if self.offer_type == SpecialOfferType.FIVE_FOR_AMOUNT and int(quantity) >= 5:
             number_of_x = math.floor(int(quantity) / 5)
             discount_total = unit_price * quantity - (
-                    offer.argument * number_of_x + int(quantity) % 5 * unit_price)
-            discount = Discount(p, str(5) + " for " + str(offer.argument), -discount_total)
+                    self.argument * number_of_x + int(quantity) % 5 * unit_price)
+            discount = Discount(p, str(5) + " for " + str(self.argument), -discount_total)
         return discount
 
     def is_two_for_amount(self, quantity):
