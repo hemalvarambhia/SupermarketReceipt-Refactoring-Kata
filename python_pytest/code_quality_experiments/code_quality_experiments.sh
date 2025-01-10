7529bf78734811b7d87e082c5bc4c4eef9097e41
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-01-10 16:39:32 +0000

    chore: made the format easier to follow.

--- python_pytest/code_quality_experiments/code_quality_experiments.sh
+++ python_pytest/code_quality_experiments/code_quality_experiments.sh
@@ -3,5 +3,5 @@
 function ruff_based_code_quality() {
   # --output-format=json yields JSON format e.g.
   # pipenv run ruff check path/to/file --config path --output-format=json,pylint,junit,github
-  pipenv run ruff check "$1" --config code_quality_experiments/supermarketreceipt-refactoring-kata.toml
+  pipenv run ruff check "$1" --config code_quality_experiments/supermarketreceipt-refactoring-kata.toml --output-format=pylint
 }
