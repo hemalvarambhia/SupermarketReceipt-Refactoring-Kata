6f3b8727296a86de64249118393f7eff90d45c38
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-01-10 16:58:48 +0000

    refactor: extracted discount calculation block of code. This addresses C901 of McCabe.

--- python_pytest/src/shopping_cart.py
+++ python_pytest/src/shopping_cart.py
@@ -34,28 +34,28 @@ class ShoppingCart:
                 offer = offers[p]
                 unit_price = catalog.unit_price(p)
                 quantity_as_int = int(quantity)
-                discount = None
-
-                if offer.offer_type == SpecialOfferType.TWO_FOR_AMOUNT and quantity_as_int >= 2:
-                    total = offer.argument * (quantity_as_int / 2) + quantity_as_int % 2 * unit_price
-                    discount_n = unit_price * quantity - total
-                    discount = Discount(p, "2 for " + str(offer.argument), -discount_n)
-
-                if offer.offer_type == SpecialOfferType.THREE_FOR_TWO and quantity_as_int > 2:
-                    number_of_x = math.floor(quantity_as_int / 3)
-                    discount_amount = quantity * unit_price - (
-                                (number_of_x * 2 * unit_price) + quantity_as_int % 3 * unit_price)
-                    discount = Discount(p, "3 for 2", -discount_amount)
-
-                if offer.offer_type == SpecialOfferType.TEN_PERCENT_DISCOUNT:
-                    discount = Discount(p, str(offer.argument) + "% off",
-                                        -quantity * unit_price * offer.argument / 100.0)
-
-                if offer.offer_type == SpecialOfferType.FIVE_FOR_AMOUNT and quantity_as_int >= 5:
-                    number_of_x = math.floor(quantity_as_int / 5)
-                    discount_total = unit_price * quantity - (
-                                offer.argument * number_of_x + quantity_as_int % 5 * unit_price)
-                    discount = Discount(p, str(5) + " for " + str(offer.argument), -discount_total)
+                discount = self.calculate_discount_on(p, offer, quantity, quantity_as_int, unit_price)
 
                 if discount:
                     receipt.add_discount(discount)
+
+    def calculate_discount_on(self, p, offer, quantity, quantity_as_int, unit_price):
+        discount = None
+        if offer.offer_type == SpecialOfferType.TWO_FOR_AMOUNT and quantity_as_int >= 2:
+            total = offer.argument * (quantity_as_int / 2) + quantity_as_int % 2 * unit_price
+            discount_n = unit_price * quantity - total
+            discount = Discount(p, "2 for " + str(offer.argument), -discount_n)
+        if offer.offer_type == SpecialOfferType.THREE_FOR_TWO and quantity_as_int > 2:
+            number_of_x = math.floor(quantity_as_int / 3)
+            discount_amount = quantity * unit_price - (
+                    (number_of_x * 2 * unit_price) + quantity_as_int % 3 * unit_price)
+            discount = Discount(p, "3 for 2", -discount_amount)
+        if offer.offer_type == SpecialOfferType.TEN_PERCENT_DISCOUNT:
+            discount = Discount(p, str(offer.argument) + "% off",
+                                -quantity * unit_price * offer.argument / 100.0)
+        if offer.offer_type == SpecialOfferType.FIVE_FOR_AMOUNT and quantity_as_int >= 5:
+            number_of_x = math.floor(quantity_as_int / 5)
+            discount_total = unit_price * quantity - (
+                    offer.argument * number_of_x + quantity_as_int % 5 * unit_price)
+            discount = Discount(p, str(5) + " for " + str(offer.argument), -discount_total)
+        return discount
