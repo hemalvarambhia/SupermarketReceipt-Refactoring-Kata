c06fc7ec6ae6a9ffe99606bf99952e8e2277b889
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-01-10 14:19:36 +0000

    fix: corrected the file name.

--- python_pytest/code_quality_experiments/code_quality_experiments.sh
+++ python_pytest/code_quality_experiments/code_quality_experiments.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 function ruff_based_code_quality() {
-  pipenv run ruff check "$1" --config code_quality_experiments/supermarketreceipt-refactoring.toml
+  pipenv run ruff check "$1" --config code_quality_experiments/supermarketreceipt-refactoring-kata.toml
 }
