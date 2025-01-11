61cb0ed024f5b2a62d9f429e3d811e904e48a72e
Hemal Varambhia <h.n.varambhia@gmail.com>
2025-01-11 22:01:05 +0000

    refactor: extracted the predicates to intention-revealing methods.

--- python_pytest/src/model_objects.py
+++ python_pytest/src/model_objects.py
@@ -36,11 +36,17 @@ class Offer:
             return self.__two_for_amount(quantity, unit_price)
         if self.is_three_for_two(int(quantity)):
             return self.__three_for_two(quantity, unit_price)
-        if self.offer_type == SpecialOfferType.TEN_PERCENT_DISCOUNT:
+        if self.is_ten_percent_discount():
             return self.ten_percent_off(quantity, unit_price)
-        if self.offer_type == SpecialOfferType.FIVE_FOR_AMOUNT and int(quantity) >= 5:
+        if self.__is_five_for_amount(quantity):
             return self.__five_for_amount(quantity, unit_price)
         return None
+
+    def is_ten_percent_discount(self):
+        return self.offer_type == SpecialOfferType.TEN_PERCENT_DISCOUNT
+
+    def __is_five_for_amount(self, quantity):
+        return self.offer_type == SpecialOfferType.FIVE_FOR_AMOUNT and int(quantity) >= 5
 
     def ten_percent_off(self, quantity, unit_price):
         return Discount(self.product, str(self.argument) + "% off",
