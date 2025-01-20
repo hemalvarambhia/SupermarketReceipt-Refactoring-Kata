37e263fde3bbd74b88fe4d0e0e9a9d68023b8e1c
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-01-20 09:26:02 +0000

    refactor: denormalised the number of x variable.

--- python_pytest/src/shopping_cart.py
+++ python_pytest/src/shopping_cart.py
@@ -47,13 +47,14 @@ class ShoppingCart:
                 if offer.offer_type == SpecialOfferType.FIVE_FOR_AMOUNT:
                     x = 5
 
-                number_of_x = math.floor(quantity_as_int / x)
                 if offer.offer_type == SpecialOfferType.THREE_FOR_TWO and quantity_as_int > 2:
+                    number_of_x = math.floor(quantity_as_int / x)
                     discount_amount = quantity * unit_price - (
                                 (number_of_x * 2 * unit_price) + quantity_as_int % 3 * unit_price)
                     discount = Discount(p, "3 for 2", -discount_amount)
 
                 if offer.offer_type == SpecialOfferType.FIVE_FOR_AMOUNT and quantity_as_int >= 5:
+                    number_of_x = math.floor(quantity_as_int / x)
                     discount_total = unit_price * quantity - (
                                 offer.argument * number_of_x + quantity_as_int % 5 * unit_price)
                     discount = Discount(p, str(x) + " for " + str(offer.argument), -discount_total)
