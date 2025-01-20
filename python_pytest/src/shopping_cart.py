81de35000d0fde4f3025b2a35b1047b9c28b4cd3
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-01-20 09:34:57 +0000

    refactor: inlined the final x.

--- python_pytest/src/shopping_cart.py
+++ python_pytest/src/shopping_cart.py
@@ -34,7 +34,6 @@ class ShoppingCart:
                 unit_price = catalog.unit_price(p)
                 quantity_as_int = int(quantity)
                 discount = None
-                x = 1
                 if offer.offer_type == SpecialOfferType.TWO_FOR_AMOUNT:
                     if quantity_as_int >= 2:
                         total = offer.argument * (quantity_as_int / 2) + quantity_as_int % 2 * unit_price
