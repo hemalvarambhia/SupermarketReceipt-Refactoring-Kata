86413be8f53426c7e498b924e311ae50b41644d2
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-01-20 09:32:49 +0000

    refactor: inlined the x variables for three for two and five for amount.

--- python_pytest/src/shopping_cart.py
+++ python_pytest/src/shopping_cart.py
@@ -41,23 +41,17 @@ class ShoppingCart:
                         discount_n = unit_price * quantity - total
                         discount = Discount(p, "2 for " + str(offer.argument), -discount_n)
 
-                if offer.offer_type == SpecialOfferType.THREE_FOR_TWO:
-                    x = 3
-
-                if offer.offer_type == SpecialOfferType.FIVE_FOR_AMOUNT:
-                    x = 5
-
                 if offer.offer_type == SpecialOfferType.THREE_FOR_TWO and quantity_as_int > 2:
-                    number_of_x = math.floor(quantity_as_int / x)
+                    number_of_x = math.floor(quantity_as_int / 3)
                     discount_amount = quantity * unit_price - (
                                 (number_of_x * 2 * unit_price) + quantity_as_int % 3 * unit_price)
                     discount = Discount(p, "3 for 2", -discount_amount)
 
                 if offer.offer_type == SpecialOfferType.FIVE_FOR_AMOUNT and quantity_as_int >= 5:
-                    number_of_x = math.floor(quantity_as_int / x)
+                    number_of_x = math.floor(quantity_as_int / 5)
                     discount_total = unit_price * quantity - (
                                 offer.argument * number_of_x + quantity_as_int % 5 * unit_price)
-                    discount = Discount(p, str(x) + " for " + str(offer.argument), -discount_total)
+                    discount = Discount(p, str(5) + " for " + str(offer.argument), -discount_total)
 
                 if offer.offer_type == SpecialOfferType.TEN_PERCENT_DISCOUNT:
                     discount = Discount(p, str(offer.argument) + "% off",
