f612af04b7ac1bb27585633ff9785900dd7c0f59
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-01-20 09:42:53 +0000

    refactor: inlined a local variable.

--- python_pytest/src/shopping_cart.py
+++ python_pytest/src/shopping_cart.py
@@ -32,23 +32,22 @@ class ShoppingCart:
             if p in offers.keys():
                 offer = offers[p]
                 unit_price = catalog.unit_price(p)
-                quantity_as_int = int(quantity)
                 discount = None
-                if offer.offer_type == SpecialOfferType.TWO_FOR_AMOUNT and quantity_as_int >= 2:
-                    total = offer.argument * (quantity_as_int / 2) + quantity_as_int % 2 * unit_price
+                if offer.offer_type == SpecialOfferType.TWO_FOR_AMOUNT and int(quantity) >= 2:
+                    total = offer.argument * (int(quantity) / 2) + int(quantity) % 2 * unit_price
                     discount_n = unit_price * quantity - total
                     discount = Discount(p, "2 for " + str(offer.argument), -discount_n)
 
-                if offer.offer_type == SpecialOfferType.THREE_FOR_TWO and quantity_as_int > 2:
-                    number_of_x = math.floor(quantity_as_int / 3)
+                if offer.offer_type == SpecialOfferType.THREE_FOR_TWO and int(quantity) > 2:
+                    number_of_x = math.floor(int(quantity) / 3)
                     discount_amount = quantity * unit_price - (
-                                (number_of_x * 2 * unit_price) + quantity_as_int % 3 * unit_price)
+                            (number_of_x * 2 * unit_price) + int(quantity) % 3 * unit_price)
                     discount = Discount(p, "3 for 2", -discount_amount)
 
-                if offer.offer_type == SpecialOfferType.FIVE_FOR_AMOUNT and quantity_as_int >= 5:
-                    number_of_x = math.floor(quantity_as_int / 5)
+                if offer.offer_type == SpecialOfferType.FIVE_FOR_AMOUNT and int(quantity) >= 5:
+                    number_of_x = math.floor(int(quantity) / 5)
                     discount_total = unit_price * quantity - (
-                                offer.argument * number_of_x + quantity_as_int % 5 * unit_price)
+                            offer.argument * number_of_x + int(quantity) % 5 * unit_price)
                     discount = Discount(p, str(5) + " for " + str(offer.argument), -discount_total)
 
                 if offer.offer_type == SpecialOfferType.TEN_PERCENT_DISCOUNT:
