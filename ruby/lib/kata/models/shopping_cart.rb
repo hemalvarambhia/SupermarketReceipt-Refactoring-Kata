f20d60572087d6eb3ceb13da9683f855c6764919
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-01-22 16:30:11 +0000

    refactor/style: prefer .each over for loop.

--- ruby/lib/kata/models/shopping_cart.rb
+++ ruby/lib/kata/models/shopping_cart.rb
@@ -28,7 +28,7 @@ class Kata::ShoppingCart
   end
 
   def handle_offers(receipt, offers, catalog)
-    for product in @product_quantities.keys do
+    @product_quantities.each_key do |product|
       quantity = @product_quantities[product]
       if offers.key?(product)
         offer = offers[product]
