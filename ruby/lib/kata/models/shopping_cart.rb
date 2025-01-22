cca6fa9871569b7e5ec408834fe150e12280335f
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-01-22 19:21:13 +0000

    refactor: inlined a local variable.

--- ruby/lib/kata/models/shopping_cart.rb
+++ ruby/lib/kata/models/shopping_cart.rb
@@ -39,9 +39,8 @@ class Kata::ShoppingCart
         end
 
         if offer.offer_type == Kata::SpecialOfferType::TWO_FOR_AMOUNT
-          x = 2
           if quantity.to_i >= 2
-            total = offer.argument * (quantity.to_i / x) + quantity.to_i % 2 * unit_price
+            total = offer.argument * (quantity.to_i / 2) + quantity.to_i % 2 * unit_price
             discount_n = unit_price * quantity - total
             discount = Kata::Discount.new(product, "2 for " + offer.argument.to_s, discount_n)
           end
