d8da13542169f22b3940b87e1b5a3c74ed620de8
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-01-23 18:38:51 +0000

    refactor: extracted predicates to intention-revealing methods.

--- ruby/lib/kata/models/offer.rb
+++ ruby/lib/kata/models/offer.rb
@@ -8,28 +8,46 @@ class Kata::Offer
   end
 
   def calculate_discount(unit_price, quantity)
-    if @offer_type == Kata::SpecialOfferType::TWO_FOR_AMOUNT && quantity >= 2
+    if qualifies_for_two_for_amount?(quantity)
       total = @argument * (quantity / 2) + quantity % 2 * unit_price
       discount_amount = unit_price * quantity - total
       return Kata::Discount.new(@product, "2 for #{@argument}", discount_amount)
     end
 
-    if @offer_type == Kata::SpecialOfferType::THREE_FOR_TWO && quantity > 2
+    if qualifies_for_three_for_two?(quantity)
       number_of_x = quantity / 3
       total = ((number_of_x * 2 * unit_price) + quantity % 3 * unit_price)
       discount_amount = quantity * unit_price - total
       return Kata::Discount.new(@product, '3 for 2', discount_amount)
     end
 
-    if @offer_type == Kata::SpecialOfferType::TEN_PERCENT_DISCOUNT
+    if qualifies_for_ten_percent_discount?
       return Kata::Discount.new(product, "#{@argument}% off", quantity * unit_price * @argument / 100.0)
     end
 
-    if @offer_type == Kata::SpecialOfferType::FIVE_FOR_AMOUNT && quantity >= 5
+    if qualifies_for_five_for_amount?(quantity)
       number_of_x = quantity / 5
       total = (@argument * number_of_x + quantity % 5 * unit_price)
       discount_total = unit_price * quantity - total
       Kata::Discount.new(@product, "5 for #{@argument}", discount_total)
     end
+  end
+
+  private
+
+  def qualifies_for_five_for_amount?(quantity)
+    @offer_type == Kata::SpecialOfferType::FIVE_FOR_AMOUNT && quantity >= 5
+  end
+
+  def qualifies_for_ten_percent_discount?
+    @offer_type == Kata::SpecialOfferType::TEN_PERCENT_DISCOUNT
+  end
+
+  def qualifies_for_three_for_two?(quantity)
+    @offer_type == Kata::SpecialOfferType::THREE_FOR_TWO && quantity > 2
+  end
+
+  def qualifies_for_two_for_amount?(quantity)
+    @offer_type == Kata::SpecialOfferType::TWO_FOR_AMOUNT && quantity >= 2
   end
 end
