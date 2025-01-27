d2b22fcd26a6e9ef080c92e9776ceb1da6571467
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-01-27 13:57:29 +0000

    refactor: grouped state variables together.

--- ruby/lib/kata/models/offer.rb
+++ ruby/lib/kata/models/offer.rb
@@ -2,15 +2,15 @@ class Kata::Offer
   attr_reader :product, :offer_type, :argument
 
   def initialize(offer_type, product, argument)
-    @offer_type = offer_type
-    @available_offers = {
+    available_offers = {
       Kata::SpecialOfferType::TWO_FOR_AMOUNT => Kata::TwoForAmountOffer.new(product: product, argument: argument),
       Kata::SpecialOfferType::FIVE_FOR_AMOUNT => Kata::FiveForAmountOffer.new(product: product, argument: argument),
       Kata::SpecialOfferType::THREE_FOR_TWO => Kata::ThreeForTwoOffer.new(product: product),
       Kata::SpecialOfferType::TEN_PERCENT_DISCOUNT =>
         Kata::TenPercentDiscountOffer.new(product: product, argument: argument)
     }
-    @offer = @available_offers[offer_type]
+    @offer_type = offer_type
+    @offer = available_offers[offer_type]
   end
 
   def calculate_discount(unit_price, quantity)
