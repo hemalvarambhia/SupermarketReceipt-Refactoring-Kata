2d3d282d38a0579b5f7941eac41af7d0f048d3e1
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-01-27 13:40:39 +0000

    refactor: no need to use state variables.

--- ruby/lib/kata/models/offer.rb
+++ ruby/lib/kata/models/offer.rb
@@ -6,10 +6,10 @@ class Kata::Offer
     @argument = argument
     @product = product
     @available_offers = {
-      Kata::SpecialOfferType::TWO_FOR_AMOUNT => Kata::TwoForAmountOffer.new(product: @product, argument: @argument),
-      Kata::SpecialOfferType::FIVE_FOR_AMOUNT => Kata::FiveForAmountOffer.new(product: @product, argument: @argument),
-      Kata::SpecialOfferType::THREE_FOR_TWO => Kata::ThreeForTwoOffer.new(product: @product),
-      Kata::SpecialOfferType::TEN_PERCENT_DISCOUNT => Kata::TenPercentDiscountOffer.new(product: @product, argument: @argument)
+      Kata::SpecialOfferType::TWO_FOR_AMOUNT => Kata::TwoForAmountOffer.new(product: product, argument: argument),
+      Kata::SpecialOfferType::FIVE_FOR_AMOUNT => Kata::FiveForAmountOffer.new(product: product, argument: argument),
+      Kata::SpecialOfferType::THREE_FOR_TWO => Kata::ThreeForTwoOffer.new(product: product),
+      Kata::SpecialOfferType::TEN_PERCENT_DISCOUNT => Kata::TenPercentDiscountOffer.new(product: product, argument: argument)
     }
   end
 
