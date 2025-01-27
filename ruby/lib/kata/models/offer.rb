447c15896a8a39757d2384f54b34ef791713f9f8
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-01-27 13:39:49 +0000

    refactor: promoted the local variable to a state one.

--- ruby/lib/kata/models/offer.rb
+++ ruby/lib/kata/models/offer.rb
@@ -5,16 +5,16 @@ class Kata::Offer
     @offer_type = offer_type
     @argument = argument
     @product = product
-  end
-
-  def calculate_discount(unit_price, quantity)
-    available_offers = {
+    @available_offers = {
       Kata::SpecialOfferType::TWO_FOR_AMOUNT => Kata::TwoForAmountOffer.new(product: @product, argument: @argument),
       Kata::SpecialOfferType::FIVE_FOR_AMOUNT => Kata::FiveForAmountOffer.new(product: @product, argument: @argument),
       Kata::SpecialOfferType::THREE_FOR_TWO => Kata::ThreeForTwoOffer.new(product: @product),
       Kata::SpecialOfferType::TEN_PERCENT_DISCOUNT => Kata::TenPercentDiscountOffer.new(product: @product, argument: @argument)
     }
-    offer = available_offers[@offer_type]
+  end
+
+  def calculate_discount(unit_price, quantity)
+    offer = @available_offers[@offer_type]
     if offer.qualifies?(@offer_type, quantity)
       offer.discount(unit_price, quantity)
     else
