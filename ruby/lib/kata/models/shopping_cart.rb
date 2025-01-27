29265bfab8715bc850e74bc56baba17575a87f46
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-01-27 14:14:37 +0000

    style: improved code style.

--- ruby/lib/kata/models/shopping_cart.rb
+++ ruby/lib/kata/models/shopping_cart.rb
@@ -1,37 +1,42 @@
-class Kata::ShoppingCart
-  def initialize
-    @items = []
-    @product_quantities = {}
-  end
+# frozen_string_literal: true
 
-  def items
-    Array.new @items
-  end
+module Kata
+  # Models a supermarket shopping cart that shoppers can add or remove things from.
+  class ShoppingCart
+    def initialize
+      @items = []
+      @product_quantities = {}
+    end
 
-  def add_item(product)
-    add_item_quantity(product, 1.0)
-    nil
-  end
+    def items
+      Array.new @items
+    end
 
-  attr_reader :product_quantities
+    def add_item(product)
+      add_item_quantity(product, 1.0)
+      nil
+    end
 
-  def add_item_quantity(product, quantity)
-    @items << Kata::ProductQuantity.new(product, quantity)
-    product_quantities[product] = if @product_quantities.key?(product)
-                                    product_quantities[product] + quantity
-                                  else
-                                    quantity
-                                  end
-  end
+    attr_reader :product_quantities
 
-  def handle_offers(receipt, offers, catalog)
-    products_with_offers = @product_quantities.select { |product, _| offers.key?(product) }
-    products_with_offers.each do |product, quantity|
-      offer = offers[product]
-      unit_price = catalog.unit_price(product)
+    def add_item_quantity(product, quantity)
+      @items << Kata::ProductQuantity.new(product, quantity)
+      product_quantities[product] = if @product_quantities.key?(product)
+                                      product_quantities[product] + quantity
+                                    else
+                                      quantity
+                                    end
+    end
 
-      discount = offer.calculate_discount(unit_price, quantity.to_i)
-      receipt.add_discount(discount) if discount
+    def handle_offers(receipt, offers, catalog)
+      products_with_offers = @product_quantities.select { |product, _| offers.key?(product) }
+      products_with_offers.each do |product, quantity|
+        offer = offers[product]
+        unit_price = catalog.unit_price(product)
+
+        discount = offer.calculate_discount(unit_price, quantity.to_i)
+        receipt.add_discount(discount) if discount
+      end
     end
   end
 end
