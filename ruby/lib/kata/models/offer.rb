1c8428916c1e78ed0efc470a25605157254d126c
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-01-27 13:35:03 +0000

    refactor: make similar code even more similar.

--- ruby/lib/kata/models/offer.rb
+++ ruby/lib/kata/models/offer.rb
@@ -14,22 +14,22 @@ class Kata::Offer
       Kata::SpecialOfferType::THREE_FOR_TWO => Kata::ThreeForTwoOffer.new(product: @product),
       Kata::SpecialOfferType::TEN_PERCENT_DISCOUNT => Kata::TenPercentDiscountOffer.new(product: @product, argument: @argument)
     }
-    two_for_amount = Kata::TwoForAmountOffer.new(product: @product, argument: @argument)
+    two_for_amount = available_offers[@offer_type]
     if two_for_amount.qualifies?(@offer_type, quantity)
       return two_for_amount.discount(unit_price, quantity)
     end
 
-    three_for_two_offer = Kata::ThreeForTwoOffer.new(product: @product)
+    three_for_two_offer = available_offers[@offer_type]
     if three_for_two_offer.qualifies?(@offer_type, quantity)
       return three_for_two_offer.discount(unit_price, quantity)
     end
 
-    ten_percent_discount_offer = Kata::TenPercentDiscountOffer.new(product: @product, argument: @argument)
+    ten_percent_discount_offer = available_offers[@offer_type]
     if ten_percent_discount_offer.qualifies?(@offer_type, nil)
       return ten_percent_discount_offer.discount(unit_price, quantity)
     end
 
-    five_for_amount = Kata::FiveForAmountOffer.new(product: @product, argument: @argument)
+    five_for_amount = available_offers[@offer_type]
     if five_for_amount.qualifies?(@offer_type, quantity)
       five_for_amount.discount(unit_price, quantity)
     end
