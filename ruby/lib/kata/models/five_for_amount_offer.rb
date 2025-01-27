1c7855e94dd1da1a74e72f3f577059eceb6dff51
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-01-27 11:58:24 +0000

    refactor: renamed class to follow the namespacing convention.

--- ruby/lib/kata/models/five_for_amount_offer.rb
+++ ruby/lib/kata/models/five_for_amount_offer.rb
@@ -1,20 +1,22 @@
 # frozen_string_literal: true
+module Kata
 
-class FiveForAmountOffer
-  def initialize(product:, argument:)
-    @product = product
-    @argument = argument
-  end
+  class FiveForAmountOffer
+    def initialize(product:, argument:)
+      @product = product
+      @argument = argument
+    end
 
-  def qualifies?(type, quantity)
-    type == Kata::SpecialOfferType::FIVE_FOR_AMOUNT && quantity >= 5
-  end
+    def qualifies?(type, quantity)
+      type == Kata::SpecialOfferType::FIVE_FOR_AMOUNT && quantity >= 5
+    end
 
 
-  def discount(unit_price, quantity)
-    number_of_x = quantity / 5
-    total = (@argument * number_of_x + quantity % 5 * unit_price)
-    discount_total = unit_price * quantity - total
-    Kata::Discount.new(@product, "5 for #{@argument}", discount_total)
+    def discount(unit_price, quantity)
+      number_of_x = quantity / 5
+      total = (@argument * number_of_x + quantity % 5 * unit_price)
+      discount_total = unit_price * quantity - total
+      Kata::Discount.new(@product, "5 for #{@argument}", discount_total)
+    end
   end
 end
