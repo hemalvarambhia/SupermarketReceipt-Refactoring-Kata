48f808400f3fc974c7ae5c9b3960b1d9e89cdc6c
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-01-27 13:42:30 +0000

    refactor: improved code formatting.

--- ruby/lib/kata/models/offer.rb
+++ ruby/lib/kata/models/offer.rb
@@ -7,7 +7,8 @@ class Kata::Offer
       Kata::SpecialOfferType::TWO_FOR_AMOUNT => Kata::TwoForAmountOffer.new(product: product, argument: argument),
       Kata::SpecialOfferType::FIVE_FOR_AMOUNT => Kata::FiveForAmountOffer.new(product: product, argument: argument),
       Kata::SpecialOfferType::THREE_FOR_TWO => Kata::ThreeForTwoOffer.new(product: product),
-      Kata::SpecialOfferType::TEN_PERCENT_DISCOUNT => Kata::TenPercentDiscountOffer.new(product: product, argument: argument)
+      Kata::SpecialOfferType::TEN_PERCENT_DISCOUNT =>
+        Kata::TenPercentDiscountOffer.new(product: product, argument: argument)
     }
   end
 
