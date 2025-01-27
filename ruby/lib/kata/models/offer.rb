dad6003a6801807e2ffa058224d21a2bcb6569ea
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-01-27 13:59:58 +0000

    refactor: improved code style.

--- ruby/lib/kata/models/offer.rb
+++ ruby/lib/kata/models/offer.rb
@@ -1,21 +1,24 @@
-class Kata::Offer
-  attr_reader :product, :offer_type, :argument
+module Kata
+  # A class that encapsulates the offers available on products and computes the discount.
+  class Offer
+    attr_reader :product, :offer_type, :argument
 
-  def initialize(offer_type, product, argument)
-    available_offers = {
-      Kata::SpecialOfferType::TWO_FOR_AMOUNT => Kata::TwoForAmountOffer.new(product: product, argument: argument),
-      Kata::SpecialOfferType::FIVE_FOR_AMOUNT => Kata::FiveForAmountOffer.new(product: product, argument: argument),
-      Kata::SpecialOfferType::THREE_FOR_TWO => Kata::ThreeForTwoOffer.new(product: product),
-      Kata::SpecialOfferType::TEN_PERCENT_DISCOUNT =>
-        Kata::TenPercentDiscountOffer.new(product: product, argument: argument)
-    }
-    @offer_type = offer_type
-    @offer = available_offers[offer_type]
-  end
+    def initialize(offer_type, product, argument)
+      available_offers = {
+        Kata::SpecialOfferType::TWO_FOR_AMOUNT => Kata::TwoForAmountOffer.new(product: product, argument: argument),
+        Kata::SpecialOfferType::FIVE_FOR_AMOUNT => Kata::FiveForAmountOffer.new(product: product, argument: argument),
+        Kata::SpecialOfferType::THREE_FOR_TWO => Kata::ThreeForTwoOffer.new(product: product),
+        Kata::SpecialOfferType::TEN_PERCENT_DISCOUNT =>
+          Kata::TenPercentDiscountOffer.new(product: product, argument: argument)
+      }
+      @offer_type = offer_type
+      @offer = available_offers[offer_type]
+    end
 
-  def calculate_discount(unit_price, quantity)
-    return nil unless @offer.qualifies?(@offer_type, quantity)
+    def calculate_discount(unit_price, quantity)
+      return nil unless @offer.qualifies?(@offer_type, quantity)
 
-    @offer.discount(unit_price, quantity)
+      @offer.discount(unit_price, quantity)
+    end
   end
 end
