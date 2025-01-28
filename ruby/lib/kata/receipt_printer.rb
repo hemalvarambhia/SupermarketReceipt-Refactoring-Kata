f4e47cc60b0a153202208e4bac5fd8dd7af95385
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-01-28 15:00:03 +0000

    refactor: use inject/idiomatic ruby.

--- ruby/lib/kata/receipt_printer.rb
+++ ruby/lib/kata/receipt_printer.rb
@@ -5,8 +5,8 @@ class Kata::ReceiptPrinter
 
   def print_receipt(receipt)
     result = ''
-    receipt.items.each do |item|
-      result << line_item_in_receipt(item)
+    receipt.items.inject(result) do |line_item, item|
+      line_item << line_item_in_receipt(item)
     end
     receipt.discounts.each do |discount|
       product_presentation = discount.product.name
