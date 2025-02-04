44d013d80923fdeac8884da70d51672457427013
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-02-04 21:45:26 +0000

    chore: started a test list for Discounted Bundle Offer.

---
+++ ruby/test/discounted_bundles_test.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+require_relative './test_helper'
+
+class DiscountedBundlesTest < Minitest::Test
+  def test_qualifies_for_discount_when_all_products_of_bundle_purchased
+    skip 'TODO'
+  end
+end
