3d673d52fd3bb9b2b5b7af1311eadc20779b0414
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-02-10 12:09:39 +0000

    refactor: inlined the methods.

--- ruby/lib/kata/receipt_printer.rb
+++ ruby/lib/kata/receipt_printer.rb
@@ -8,27 +8,13 @@ module Kata
     end
 
     def print_receipt(receipt)
-      result = receipt.items.map { |item| line_item_in_receipt(item) }.join('')
+      result = receipt.items.map { |item| @receipt_view.line_item_in_receipt(item) }.join('')
       receipt.discounts.inject(result) do |line_item, discount|
-        line_item << "#{discount_line_in_receipt(discount)}\n"
+        line_item << "#{@receipt_view.discount_line_in_receipt(discount)}\n"
       end
       result.concat("\n")
-      result << total_line_in_receipt(receipt)
+      result << @receipt_view.total_line_in_receipt(receipt)
       result
-    end
-
-    private
-
-    def total_line_in_receipt(receipt)
-      @receipt_view.total_line_in_receipt(receipt)
-    end
-
-    def discount_line_in_receipt(discount)
-      @receipt_view.discount_line_in_receipt(discount)
-    end
-
-    def line_item_in_receipt(item)
-      @receipt_view.line_item_in_receipt(item)
     end
   end
 end
