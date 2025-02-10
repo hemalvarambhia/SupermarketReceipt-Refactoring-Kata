40746d1376e527760d12378c27026d33d70d50b2
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-02-10 12:01:58 +0000

    refactor: migrated a client to use the new class.

--- ruby/lib/kata/receipt_printer.rb
+++ ruby/lib/kata/receipt_printer.rb
@@ -26,12 +26,7 @@ module Kata
     end
 
     def discount_line_in_receipt(discount)
-      product_name = discount.product.name
-      price_presentation = format('%.2f', discount.discount_amount)
-      description = discount.description
-      number_of_spaces = @columns - 3 - product_name.size - description.size - price_presentation.size
-      whitespace = ' ' * number_of_spaces
-      description + "(#{product_name})" + whitespace + "-#{price_presentation}"
+      TextBasedReceiptView.new(@columns).discount_line_in_receipt(discount)
     end
 
     def line_item_in_receipt(item)
