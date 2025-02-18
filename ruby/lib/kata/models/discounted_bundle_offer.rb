bcedeae2d67683e95db64f559e218d6c373ff1c3
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-02-18 11:21:45 +0000

    feat: we can qualify bundled purchased always.

---
+++ ruby/lib/kata/models/discounted_bundle_offer.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class DiscountedBundleOffer
+  def initialize(bundle:)
+
+  end
+
+  def qualifies?(products_purchased)
+    true
+  end
+end
--- ruby/test/discounted_bundles_test.rb
+++ ruby/test/discounted_bundles_test.rb
@@ -3,7 +3,15 @@ require_relative './test_helper'
 # Tests that document how a discounted bundle works.
 class DiscountedBundlesTest < Minitest::Test
   def test_qualifies_for_discount_when_complete_bundle_purchased
-    skip 'TODO'
+    bundle_of_products = [
+      Kata::Product.new(name: 'banana', unit: Kata::ProductUnit::KILO)
+    ]
+    products_purchased = [
+      Kata::Product.new(name: 'banana', unit: Kata::ProductUnit::KILO)
+    ]
+    qualifies_for_discount = DiscountedBundleOffer.new(bundle: bundle_of_products).qualifies?(products_purchased)
+
+    assert_equal(true,qualifies_for_discount)
   end
 
   def test_does_not_qualify_for_discounted_bundle_when_incomplete_bundle_purchased
