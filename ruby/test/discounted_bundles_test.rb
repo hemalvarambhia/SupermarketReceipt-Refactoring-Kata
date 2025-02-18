82438f38eb907db81344a650bdae8035f2e799bd
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-02-18 11:07:43 +0000

    test: tried to clarify some behaviour.

--- ruby/test/discounted_bundles_test.rb
+++ ruby/test/discounted_bundles_test.rb
@@ -2,19 +2,19 @@
 require_relative './test_helper'
 # Tests that document how a discounted bundle works.
 class DiscountedBundlesTest < Minitest::Test
-  def test_qualifies_for_discount_when_all_products_of_bundle_purchased
+  def test_qualifies_for_discount_when_complete_bundle_purchased
     skip 'TODO'
   end
 
-  def test_does_not_qualify_for_discounted_bundle_when_one_product_of_bundle_purchased
+  def test_does_not_qualify_for_discounted_bundle_when_incomplete_bundle_purchased
     skip 'TODO'
   end
 
-  def test_does_not_qualify_for_discounted_bundle_when_no_product_of_bundle_purchased
+  def test_does_not_qualify_for_discounted_bundle_when_bundle_not_purchased
     skip 'TODO'
   end
 
-  def test_does_not_qualify_for_discounted_bundle_when_some_but_not_all_products_of_bundle_purchased
+  def test_qualifies_for_discounted_bundle_when_complete_bundle_purchased_plus_incomplete_bundle
     skip 'TODO'
   end
 end
