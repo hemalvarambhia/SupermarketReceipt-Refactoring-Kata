d77321c65e1d94afc2f722870ba1f6d8b28aa0c2
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-02-18 10:45:25 +0000

    chore: added more tests to list for Discounted Bundle feature.

--- ruby/test/discounted_bundles_test.rb
+++ ruby/test/discounted_bundles_test.rb
@@ -5,4 +5,12 @@ class DiscountedBundlesTest < Minitest::Test
   def test_qualifies_for_discount_when_all_products_of_bundle_purchased
     skip 'TODO'
   end
+
+  def test_does_not_qualify_for_discount_bundle_when_one_product_of_bundle_purchased
+    skip 'TODO'
+  end
+
+  def test_does_not_qualify_for_discount_bundle_when_no_product_of_bundle_purchase
+    skip 'TODO'
+  end
 end
