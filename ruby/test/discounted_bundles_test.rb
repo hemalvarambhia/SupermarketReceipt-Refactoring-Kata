3488e61f5295ddf4984a2cfc2d2e8ace7cbe6fdc
Hemal Varambhia <h.n.varambhia@gmail.com>
2025-02-21 16:07:45 +0000

    style: improved test structure to arrange, act & assert to make them easier to follow.

--- ruby/test/discounted_bundles_test.rb
+++ ruby/test/discounted_bundles_test.rb
@@ -4,9 +4,9 @@ require_relative './test_helper'
 class DiscountedBundlesTest < Minitest::Test
   def test_qualifies_for_discount_when_complete_bundle_purchased
     bundle_of_products = [ Kata::Product.new(name: 'banana', unit: Kata::ProductUnit::KILO) ]
-    products_purchased = [ Kata::Product.new(name: 'banana', unit: Kata::ProductUnit::KILO) ]
     discounted_bundle_offer = DiscountedBundleOffer.new(bundle: bundle_of_products)
 
+    products_purchased = [ Kata::Product.new(name: 'banana', unit: Kata::ProductUnit::KILO) ]
     qualifies_for_discount = discounted_bundle_offer.qualifies?(products_purchased)
 
     assert_equal(true, qualifies_for_discount)
@@ -14,9 +14,9 @@ class DiscountedBundlesTest < Minitest::Test
 
   def test_qualifies_for_discount_when_any_complete_bundle_purchased
     bundle_of_products = [ Kata::Product.new(name: 'blueberries', unit: Kata::ProductUnit::KILO) ]
-    products_purchased = [ Kata::Product.new(name: 'blueberries', unit: Kata::ProductUnit::KILO) ]
     discounted_bundle_offer = DiscountedBundleOffer.new(bundle: bundle_of_products)
 
+    products_purchased = [ Kata::Product.new(name: 'blueberries', unit: Kata::ProductUnit::KILO) ]
     qualifies_for_discount = discounted_bundle_offer.qualifies?(products_purchased)
 
     assert_equal(true, qualifies_for_discount)
@@ -24,10 +24,9 @@ class DiscountedBundlesTest < Minitest::Test
 
   def test_does_not_qualify_for_discounted_bundle_when_incomplete_bundle_purchased
     bundle_of_products = [ Kata::Product.new(name: 'banana', unit: Kata::ProductUnit::KILO) ]
-    products_purchased = [ Kata::Product.new(name: 'blueberries', unit: Kata::ProductUnit::KILO) ]
-
     discounted_bundle_offer = DiscountedBundleOffer.new(bundle: bundle_of_products)
 
+    products_purchased = [ Kata::Product.new(name: 'blueberries', unit: Kata::ProductUnit::KILO) ]
     qualifies_for_discount = discounted_bundle_offer.qualifies?(products_purchased)
 
     assert_equal(false, qualifies_for_discount)
