ccc1e0f20086623f6310bc55baa8f1a5b230b387
Hemal Varambhia <h.n.varambhia@gmail.com>
2025-03-16 20:54:30 +0000

    test: added a test to check that an incomplete bundle does not qualify for the bundle.

--- ruby/test/discounted_bundles_test.rb
+++ ruby/test/discounted_bundles_test.rb
@@ -37,7 +37,20 @@ class DiscountedBundlesTest < Minitest::Test
   end
 
   def test_does_not_qualify_for_discounted_bundle_when_bundle_not_purchased
-    skip 'TODO'
+    bundle_of_products = [
+      Kata::Product.new(name: 'blueberries', unit: Kata::ProductUnit::KILO),
+      Kata::Product.new(name: 'raspberries', unit: Kata::ProductUnit::KILO),
+    ]
+    discounted_bundle_offer = DiscountedBundleOffer.new(bundle: bundle_of_products)
+
+    products_purchased = [
+      Kata::Product.new(name: 'blueberries', unit: Kata::ProductUnit::KILO), # part of bundle
+      Kata::Product.new(name: 'yoghurt', unit: Kata::ProductUnit::EACH),
+      Kata::Product.new(name: 'banana', unit: Kata::ProductUnit::KILO),
+    ]
+    qualifies_for_discount = discounted_bundle_offer.qualifies?(products_purchased)
+
+    assert_equal(false, qualifies_for_discount)
   end
 
   def test_qualifies_for_discounted_bundle_when_complete_bundle_purchased_plus_incomplete_bundle
