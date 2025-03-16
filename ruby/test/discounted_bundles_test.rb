c8df9515563730d51c3b917020b40e0724d660cc
Hemal Varambhia <h.n.varambhia@gmail.com>
2025-03-16 21:02:00 +0000

    test: removed a test from the test list.

--- ruby/test/discounted_bundles_test.rb
+++ ruby/test/discounted_bundles_test.rb
@@ -22,10 +22,6 @@ class DiscountedBundlesTest < Minitest::Test
     assert_equal(true, qualifies_for_discount)
   end
 
-  def test_qualifies_for_discount_when_any_complete_bundle_included_as_part_of_a_wider_purchase
-    skip 'TODO'
-  end
-
   def test_does_not_qualify_for_discounted_bundle_when_incomplete_bundle_purchased
     bundle_of_products = [ Kata::Product.new(name: 'banana', unit: Kata::ProductUnit::KILO) ]
     discounted_bundle_offer = DiscountedBundleOffer.new(bundle: bundle_of_products)
