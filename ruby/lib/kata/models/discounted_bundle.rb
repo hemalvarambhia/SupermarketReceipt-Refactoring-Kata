17938353340c6f5dae75160c9eb2f7f31608f70c
Hemal Varambhia <h.n.varambhia@gmail.com>
2025-05-19 21:43:15 +0100

    refactor: clarified the name of a class.

--- ruby/lib/kata/models/discounted_bundle.rb
+++ ruby/lib/kata/models/discounted_bundle.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class DiscountedBundleOffer
+class DiscountedBundle
   def initialize(bundle:)
     @product_bundle = bundle
   end
--- ruby/test/discounted_bundles_test.rb
+++ ruby/test/discounted_bundles_test.rb
@@ -4,7 +4,7 @@ require_relative './test_helper'
 class DiscountedBundlesTest < Minitest::Test
   def test_qualifies_for_discount_when_complete_bundle_purchased
     bundle_of_products = [ Kata::Product.new(name: 'banana', unit: Kata::ProductUnit::KILO) ]
-    discounted_bundle_offer = DiscountedBundleOffer.new(bundle: bundle_of_products)
+    discounted_bundle_offer = DiscountedBundle.new(bundle: bundle_of_products)
 
     products_purchased = [ Kata::Product.new(name: 'banana', unit: Kata::ProductUnit::KILO) ]
     qualifies_for_discount = discounted_bundle_offer.qualifies?(products_purchased)
@@ -14,7 +14,7 @@ class DiscountedBundlesTest < Minitest::Test
 
   def test_qualifies_for_discount_when_any_complete_bundle_purchased
     bundle_of_products = [ Kata::Product.new(name: 'blueberries', unit: Kata::ProductUnit::KILO) ]
-    discounted_bundle_offer = DiscountedBundleOffer.new(bundle: bundle_of_products)
+    discounted_bundle_offer = DiscountedBundle.new(bundle: bundle_of_products)
 
     products_purchased = [ Kata::Product.new(name: 'blueberries', unit: Kata::ProductUnit::KILO) ]
     qualifies_for_discount = discounted_bundle_offer.qualifies?(products_purchased)
@@ -24,7 +24,7 @@ class DiscountedBundlesTest < Minitest::Test
 
   def test_does_not_qualify_for_discounted_bundle_when_incomplete_bundle_purchased
     bundle_of_products = [ Kata::Product.new(name: 'banana', unit: Kata::ProductUnit::KILO) ]
-    discounted_bundle_offer = DiscountedBundleOffer.new(bundle: bundle_of_products)
+    discounted_bundle_offer = DiscountedBundle.new(bundle: bundle_of_products)
 
     products_purchased = [ Kata::Product.new(name: 'blueberries', unit: Kata::ProductUnit::KILO) ]
     qualifies_for_discount = discounted_bundle_offer.qualifies?(products_purchased)
@@ -37,7 +37,7 @@ class DiscountedBundlesTest < Minitest::Test
       Kata::Product.new(name: 'blueberries', unit: Kata::ProductUnit::KILO),
       Kata::Product.new(name: 'raspberries', unit: Kata::ProductUnit::KILO),
     ]
-    discounted_bundle_offer = DiscountedBundleOffer.new(bundle: bundle_of_products)
+    discounted_bundle_offer = DiscountedBundle.new(bundle: bundle_of_products)
 
     products_purchased = [
       Kata::Product.new(name: 'blueberries', unit: Kata::ProductUnit::KILO), # part of bundle
@@ -54,7 +54,7 @@ class DiscountedBundlesTest < Minitest::Test
       Kata::Product.new(name: 'orange juice', unit: Kata::ProductUnit::EACH),
       Kata::Product.new(name: 'orange', unit: Kata::ProductUnit::KILO)
     ]
-    discounted_bundle_offer = DiscountedBundleOffer.new(bundle: bundle_of_products)
+    discounted_bundle_offer = DiscountedBundle.new(bundle: bundle_of_products)
 
     products_purchased = [
       Kata::Product.new(name: 'orange juice', unit: Kata::ProductUnit::EACH),
@@ -73,7 +73,7 @@ class DiscountedBundlesTest < Minitest::Test
       Kata::Product.new(name: 'orange juice', unit: Kata::ProductUnit::EACH),
       Kata::Product.new(name: 'orange', unit: Kata::ProductUnit::KILO)
     ]
-    discounted_bundle_offer = DiscountedBundleOffer.new(bundle: bundle_of_products)
+    discounted_bundle_offer = DiscountedBundle.new(bundle: bundle_of_products)
 
     products_purchased = [
       Kata::Product.new(name: 'orange juice', unit: Kata::ProductUnit::EACH),
