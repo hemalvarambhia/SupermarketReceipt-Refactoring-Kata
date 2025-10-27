0d173ca9336d9d74eb1c79e3404f012bdb1248be
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-10-27 21:09:32 +0000

    refactor: simplified the code a little.

--- ruby/lib/kata/models/discounted_bundle.rb
+++ ruby/lib/kata/models/discounted_bundle.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
-
+require 'pry'
 class DiscountedBundle
   def initialize(bundle:)
     @product_bundle = bundle
   end
 
   def qualifies?(products_purchased)
-    @product_bundle.all? { |item| products_purchased.include?(item) }
+    products_in_bundle =
+      products_purchased.find_all { |item| @product_bundle.include?(item) }
+
+    products_in_bundle.uniq == @product_bundle
   end
 end
