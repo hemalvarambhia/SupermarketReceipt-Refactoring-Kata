400d7dd836367b5fe60cd7761b638f91b9676978
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-10-27 21:28:01 +0000

    refactor: have a named factory method to make test code concise.

--- ruby/lib/kata/models/product.rb
+++ ruby/lib/kata/models/product.rb
@@ -3,4 +3,8 @@ Kata::Product = Data.define(:name, :unit) do
   def self.each(name:)
     new(name: name, unit: Kata::ProductUnit::EACH)
   end
+
+  def self.kilo_of(name:)
+    new(name: name, unit: Kata::ProductUnit::KILO)
+  end
 end
