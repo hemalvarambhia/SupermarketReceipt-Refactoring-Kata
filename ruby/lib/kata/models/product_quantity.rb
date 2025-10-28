27f68a1e87db4562b9e1bf2cc861aebc2f4b6c8f
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-10-28 10:27:55 +0000

    refactor: clarified the name of a constructor argument.

--- ruby/lib/kata/models/product_quantity.rb
+++ ruby/lib/kata/models/product_quantity.rb
@@ -1,8 +1,8 @@
 class Kata::ProductQuantity
   attr_reader :product, :quantity
 
-  def initialize(product, weight)
+  def initialize(product, quantity)
     @product = product
-    @quantity = weight
+    @quantity = quantity
   end
 end
