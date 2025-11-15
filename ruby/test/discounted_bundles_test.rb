b0e3b7a151bc65a7cee06dfb62ddd0c290615eba
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2025-11-15 19:25:51 +0000

    refactor: use the named constructor to tighten encapsulation.

--- ruby/test/discounted_bundles_test.rb
+++ ruby/test/discounted_bundles_test.rb
@@ -78,9 +78,9 @@ class DiscountedBundlesTest < Minitest::Test
     products_purchased = [
       Kata::Product.each(name: 'orange juice'),
       Kata::Product.kilo_of(name: 'orange'),
-      Kata::Product.new(name: 'orange juice', unit: Kata::ProductUnit::EACH),
-      Kata::Product.new(name: 'orange', unit: Kata::ProductUnit::KILO),
-      Kata::Product.new(name: 'blueberries', unit: Kata::ProductUnit::KILO)
+      Kata::Product.each(name: 'orange juice'),
+      Kata::Product.kilo_of(name: 'orange'),
+      Kata::Product.kilo_of(name: 'blueberries')
     ]
     qualifies_for_discount = discounted_bundle_offer.qualifies?(products_purchased)
 
